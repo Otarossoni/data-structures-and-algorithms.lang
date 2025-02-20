9e807fd8b3fc0f71b824a01ca253060ad1667edc
Otarossoni <otarossoni@gmail.com>
2025-02-20 00:04:30 -0300

    feat: adiciona problema 1003 do beecrowd

---
+++ exercises/beecrowd/1003/1003.go
@@ -0,0 +1,16 @@
+// https://judge.beecrowd.com/pt/problems/view/1003
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	var b int
+
+	fmt.Scanln(&a)
+	fmt.Scanln(&b)
+
+	fmt.Println("SOMA =", (a + b))
+}
---
+++ exercises/beecrowd/1003/1003.ts
@@ -0,0 +1,10 @@
+// https://judge.beecrowd.com/pt/problems/view/1003
+import * as fs from 'fs';
+
+const input = fs.readFileSync('/dev/stdin', 'utf8');
+const lines = input.split(/\r?\n/);
+
+const a = Number(lines.shift());
+const b = Number(lines.shift());
+
+console.log(`SOMA = ${a + b}`);
