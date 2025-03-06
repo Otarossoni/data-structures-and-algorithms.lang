8389057dd971fa3d764e13449185de1671ba3f16
Otarossoni <otarossoni@gmail.com>
2025-03-06 00:04:50 -0300

    feat: adiciona problema 1 do leetcode

---
+++ exercises/leetcode/1-two-sum/1-two-sum.go
@@ -0,0 +1,15 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	pMap := map[int]int{}
+
+	for i, n := range nums {
+		diff := target - n
+		if j, targetFound := pMap[diff]; targetFound {
+			return []int{j, i}
+		}
+		pMap[n] = i
+	}
+
+	return []int{}
+}
---
+++ exercises/leetcode/1-two-sum/1-two-sum.ts
@@ -0,0 +1,16 @@
+function twoSum(nums: number[], target: number): number[] {
+    const pMap: Record<number, number> = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const n = nums[i];
+        const diff = target - n;
+
+        if (diff in pMap) {
+            return [pMap[diff], i];
+        }
+
+        pMap[n] = i;
+    }
+
+    return [];
+}
