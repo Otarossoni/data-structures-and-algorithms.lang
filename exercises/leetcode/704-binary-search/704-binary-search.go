54b2e72a24d255d0a5fe9d6b748f595793f1c452
Otarossoni <otarossoni@gmail.com>
2025-03-07 00:10:50 -0300

    feat: adiciona problema 704 do leetcode

---
+++ exercises/leetcode/704-binary-search/704-binary-search.go
@@ -0,0 +1,23 @@
+package main
+
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		medium := (low + high) / 2
+		value := nums[medium]
+
+		if value == target {
+			return medium
+		}
+
+		if value > target {
+			high = medium - 1
+		} else {
+			low = medium + 1
+		}
+	}
+
+	return -1
+}
