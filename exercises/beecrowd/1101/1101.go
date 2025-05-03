2ad9b3e76a426f8c760fea8d03be154243e7dcc0
Otarossoni <otarossoni@gmail.com>
2025-05-03 02:07:52 -0300

    feat: adiciona problema 1101 do beecrowd em Go

---
+++ exercises/beecrowd/1101/1101.go
@@ -0,0 +1,26 @@
+// https://judge.beecrowd.com/en/problems/view/1101
+package main
+
+import "fmt"
+
+func main() {
+	var m, n int
+	for {
+		fmt.Scan(&m, &n)
+
+		if m <= 0 || n <= 0 {
+			break
+		}
+
+		if m > n {
+			m, n = n, m
+		}
+
+		sum := 0
+		for i := m; i <= n; i++ {
+			fmt.Printf("%d ", i)
+			sum += i
+		}
+		fmt.Printf("Sum=%d\n", sum)
+	}
+}
