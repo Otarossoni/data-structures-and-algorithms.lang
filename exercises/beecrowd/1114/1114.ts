296276fff1660e5b71d2156e3ca30d0a230bf5ab
Otarossoni <otarossoni@gmail.com>
2025-05-08 12:00:00 -0300

    feat: adiciona problema 1115 do beecrowd em Typescript

--- exercises/beecrowd/1114/1114.ts
+++ exercises/beecrowd/1114/1114.ts
@@ -12,7 +12,6 @@ function main(): void {
       console.log("Senha Invalida");
     }
   }
-
 }
 
 main();
---
+++ exercises/beecrowd/1115/1115.ts
@@ -0,0 +1,22 @@
+// https://judge.beecrowd.com/pt/problems/view/1114
+import * as fs from "fs";
+
+function main(): void {
+  const lines = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+
+for (const line of lines) {
+  const [x, y] = line.split(' ').map(Number);
+  if (x === 0 || y === 0) break;
+    if (x > 0 && y > 0) {
+      console.log("primeiro");
+    } else if (x < 0 && y > 0) {
+      console.log("segundo");
+    } else if (x < 0 && y < 0) {
+      console.log("terceiro");
+    } else if (x > 0 && y < 0) {
+      console.log("quarto");
+    }
+  }
+}
+
+main();
