0187726df177c1ff2220899f6b80a5cf9192754f
Otarossoni <otarossoni@gmail.com>
2025-05-09 12:00:00 -0300

    feat: adiciona problema 1115 do beecrowd em Go

---
+++ exercises/beecrowd/1115/1115.go
@@ -0,0 +1,24 @@
+// https://judge.beecrowd.com/pt/problems/view/1114
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+	for {
+		fmt.Scan(&x, &y)
+		if x == 0 || y == 0 {
+			break
+		}
+
+		if x > 0 && y > 0 {
+			fmt.Println("primeiro")
+		} else if x < 0 && y > 0 {
+			fmt.Println("segundo")
+		} else if x < 0 && y < 0 {
+			fmt.Println("terceiro")
+		} else if x > 0 && y < 0 {
+			fmt.Println("quarto")
+		}
+	}
+}
