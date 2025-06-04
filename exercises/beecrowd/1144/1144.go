fea8e59a3a1ea56fcf79f87ee8573ea0230f52e8
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-06-04 12:00:00 -0300

    feat: adiciona problema 1144 do beecrowd em Go

---
+++ exercises/beecrowd/1144/1144.go
@@ -0,0 +1,14 @@
+// https://judge.beecrowd.com/pt/problems/view/1144
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	for i := 1; i <= n; i++ {
+		fmt.Printf("%d %d %d\n", i, i*i, i*i*i)
+		fmt.Printf("%d %d %d\n", i, i*i+1, i*i*i+1)
+	}
+}
