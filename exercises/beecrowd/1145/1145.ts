0e731c8e345e57ed32791b85c351d3da17f2d7b3
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-06-05 09:53:17 -0300

    feat: adiciona problema 1145 do beecrowd em Typescript

---
+++ exercises/beecrowd/1145/1145.ts
@@ -0,0 +1,28 @@
+// https://judge.beecrowd.com/pt/problems/view/1144
+import * as fs from "fs";
+
+function main(): void {
+  const input = fs
+    .readFileSync("/dev/stdin", "utf8")
+    .trim()
+    .split(" ")
+    .map(Number);
+  const x = input[0];
+  const y = input[1];
+
+  let output = "";
+
+  for (let i = 1; i <= y; i++) {
+    output += i;
+
+    if (i % x === 0) {
+      output += "\n";
+    } else {
+      output += " ";
+    }
+  }
+
+  console.log(output.trim());
+}
+
+main();
