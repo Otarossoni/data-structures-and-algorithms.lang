e305906bf15d01a63169285255cd9363fdf50ad7
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-08-02 14:15:57 -0300

    feat: adiciona problema 1186 do beecrowd em Go

---
+++ exercises/beecrowd/1186/1186.go
@@ -0,0 +1,37 @@
+// https://judge.beecrowd.com/pt/problems/view/1186
+package main
+
+import "fmt"
+
+func main() {
+	var op string
+	fmt.Scan(&op)
+
+	matrix := make([][]float64, 12)
+	for i := 0; i < 12; i++ {
+		matrix[i] = make([]float64, 12)
+		for j := 0; j < 12; j++ {
+			fmt.Scan(&matrix[i][j])
+		}
+	}
+
+	sum := 0.0
+	count := 0
+
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 12; j++ {
+			if j > 11-i {
+				sum += matrix[i][j]
+				count++
+			}
+		}
+	}
+
+	if op == "M" {
+		if count > 0 {
+			sum /= float64(count)
+		}
+	}
+
+	fmt.Printf("%.1f\n", sum)
+}
