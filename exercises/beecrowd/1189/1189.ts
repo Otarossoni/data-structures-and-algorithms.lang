cfc3e5d1b8b1d1090df4ce4c8e202fc40eda3abf
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-08-07 10:22:43 -0300

    feat: adiciona problema 1189 do beecrowd em Typescript

---
+++ exercises/beecrowd/1189/1189.ts
@@ -0,0 +1,28 @@
+// https://judge.beecrowd.com/pt/problems/view/1189
+import * as fs from "fs";
+
+function main(): void {
+  const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+  const operation = input[0];
+  const values = input.slice(1).map(Number);
+
+  let sum = 0;
+  let count = 0;
+
+  for (let i = 1; i < 11; i++) {
+    const limit = Math.min(i, 11 - i);
+
+    for (let j = 0; j < limit; j++) {
+      sum += values[i * 12 + j];
+      count++;
+    }
+  }
+
+  if (operation === "M") {
+    sum /= count;
+  }
+
+  console.log(sum.toFixed(1));
+}
+
+main();
