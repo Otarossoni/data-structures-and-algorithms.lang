03715946cc518e00892d84309fc59fc35d76c4e8
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-08-15 12:00:00 -0300

    feat: adiciona problema 1435 do beecrowd em Typescript

---
+++ exercises/beecrowd/1435/1435.ts
@@ -0,0 +1,45 @@
+// https://judge.beecrowd.com/pt/problems/view/1435
+import * as fs from "fs";
+
+function main(): void {
+  const input = fs
+    .readFileSync("/dev/stdin", "utf8")
+    .trim()
+    .split("\n")
+    .map(Number);
+  let lineIndex = 0;
+
+  while (true) {
+    const n = input[lineIndex++];
+    if (n === 0) {
+      break;
+    }
+
+    const matrix: number[][] = [];
+    for (let i = 0; i < n; i++) {
+      matrix[i] = [];
+      for (let j = 0; j < n; j++) {
+        const topDist = i;
+        const bottomDist = n - 1 - i;
+        const leftDist = j;
+        const rightDist = n - 1 - j;
+        const value = Math.min(topDist, bottomDist, leftDist, rightDist) + 1;
+        matrix[i][j] = value;
+      }
+    }
+
+    for (let i = 0; i < n; i++) {
+      let rowString = "";
+      for (let j = 0; j < n; j++) {
+        rowString += String(matrix[i][j]).padStart(3, " ");
+        if (j < n - 1) {
+          rowString += " ";
+        }
+      }
+      console.log(rowString);
+    }
+    console.log();
+  }
+}
+
+main();
