023161dbceffae83aeb337ce947d3b3802b28de9
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-09-24 12:00:00 -0300

    feat: adiciona problema 1005 do beecrowd em Java

---
+++ exercises/beecrowd/1005/1005.java
@@ -0,0 +1,16 @@
+// https://judge.beecrowd.com/pt/problems/view/1005
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        double media = (a * 3.5 + b * 7.5) / 11.0;
+        System.out.printf("MEDIA = %.5f%n", media);
+        
+        sc.close();
+    }
+}
