a356833d66b2e90f6e4a8f64a269c3c7385bec90
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-09-27 12:00:00 -0300

    feat: adiciona problema 1008 do beecrowd em Java

---
+++ exercises/beecrowd/1008/1008.java
@@ -0,0 +1,18 @@
+// https://judge.beecrowd.com/pt/problems/view/1008
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int hours = sc.nextInt();
+        double value = sc.nextDouble();
+
+        double salary = hours * value;
+        System.out.println("NUMBER = " + number);
+        System.out.printf("SALARY = U$ %.2f%n", salary);
+        
+        sc.close();
+    }
+}
