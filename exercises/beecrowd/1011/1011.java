a144b173e26670079d8874ff043eb792240ae794
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-09-30 12:00:00 -0300

    feat: adiciona problema 1011 do beecrowd em Java

---
+++ exercises/beecrowd/1011/1011.java
@@ -0,0 +1,16 @@
+// https://judge.beecrowd.com/pt/problems/view/1011
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double r = sc.nextDouble();
+        double pi = 3.14159;
+
+        double volume = (4.0 / 3.0) * pi * r * r * r;
+        System.out.printf("VOLUME = %.3f%n", volume);
+        
+        sc.close();
+    }
+}
