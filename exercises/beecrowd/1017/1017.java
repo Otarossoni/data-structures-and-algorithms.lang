84dfacf001576f5531a0eaa5b4d42feeacb17424
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-10-13 12:00:00 -0300

    feat: adiciona problema 1017 do beecrowd em Java

---
+++ exercises/beecrowd/1017/1017.java
@@ -0,0 +1,18 @@
+// https://judge.beecrowd.com/pt/problems/view/1017
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int time = sc.nextInt();
+        int speed = sc.nextInt();
+
+        double distance = time * speed;
+        double liters = distance / 12.0;
+
+        System.out.printf("%.3f%n", liters);
+
+        sc.close();
+    }
+}
