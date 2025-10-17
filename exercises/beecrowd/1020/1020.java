caa3e4853e487fc5b64a52f6e53047ca9df33d26
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-10-17 08:41:51 -0300

    feat: adiciona problema 1020 do beecrowd em Java

---
+++ exercises/beecrowd/1020/1020.java
@@ -0,0 +1,22 @@
+// https://judge.beecrowd.com/pt/problems/view/1020
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int days = sc.nextInt();
+
+        int years = days / 365;
+        days %= 365;
+
+        int months = days / 30;
+        days %= 30;
+
+        System.out.println(years + " ano(s)");
+        System.out.println(months + " mes(es)");
+        System.out.println(days + " dia(s)");
+
+        sc.close();
+    }
+}
