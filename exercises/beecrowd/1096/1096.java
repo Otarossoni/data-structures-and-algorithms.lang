bb580d578e6e438b0f3cca7b850a436cb80c36cf
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-11-25 12:00:00 -0300

    feat: adiciona problema 1096 do beecrowd em Java

---
+++ exercises/beecrowd/1096/1096.java
@@ -0,0 +1,10 @@
+// https://judge.beecrowd.com/pt/problems/view/1096
+public class Main {
+    public static void main(String[] args) {
+        for (int I = 1; I <= 9; I += 2) {
+            for (int J = 7; J >= 5; J--) {
+                System.out.println("I=" + I + " J=" + J);
+            }
+        }
+    }
+}
