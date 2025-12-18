6e110a95c634de98254b5a7852a141c9a2266752
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-12-18 12:00:00 -0300

    feat: adiciona problema 1151 do beecrowd em Java

---
+++ exercises/beecrowd/1151/1151.java
@@ -0,0 +1,21 @@
+// https://judge.beecrowd.com/pt/problems/view/1151
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == n) System.out.println(a);
+            else System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        sc.close();
+    }
+}
